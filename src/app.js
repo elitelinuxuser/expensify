a9de4decee46894bb5ed8e6a9b9d9ce503d82ed4
Lucifer <ramanandsirvi21@gmail.com>
2018-07-20 21:58:28 +0530

    Formatted

--- src/app.js
+++ src/app.js
@@ -1,159 +1,152 @@
-class IndecisionApp extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAppOption = this.handleAppOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
-            options: props.options
-        };
-    }
-    handleDeleteOptions(){
-        this.setState(()=>({options: []}));
+class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAppOption = this.handleAppOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    this.state = {
+      options: props.options
+    };
+  }
+  handleDeleteOptions() {
+    this.setState(() => ({ options: [] }));
+  }
+
+  handleDeleteOption(optionToRemove) {
+    this.setState(prevState => ({
+      options: prevState.options.filter(option => optionToRemove !== option)
+    }));
+  }
+
+  handlePick() {
+    const rand = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[rand];
+    alert(option);
+  }
+  handleAppOption(option) {
+    if (!option) {
+      return "Enter valid value to add item";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This option already exists";
     }
 
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState)=> ({
-            options: prevState.options.filter(option=> optionToRemove !== option)
-        }));
-    }
-
-    handlePick() {
-        const rand = Math.floor(Math.random()*(this.state.options.length));
-        const option = this.state.options[rand]
-        alert(option);
-    }
-    handleAppOption(option){
-        if(!option){
-            return 'Enter valid value to add item';
-        } else if (this.state.options.indexOf(option)> -1){
-            return 'This option already exists';
-        }
-
-        this.setState((prevState)=>({ options: prevState.options.concat(option) }));
-    }
-    render() {
-        const subtitle = 'Put your life in hands of a Computer!';
-        return (
-            <div>
-                <Header subtitle={subtitle}/>
-                <Action 
-                    hasOptions={this.state.options.length > 0} 
-                    handlePick = {this.handlePick}
-                />
-                <Options 
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions} 
-                    handleDeleteOption = {this.handleDeleteOption}
-                />
-                <AddOption 
-                    handleAppOption = {this.handleAppOption}
-                />
-            </div>
-        );
-    }
+    this.setState(prevState => ({ options: prevState.options.concat(option) }));
+  }
+  render() {
+    const subtitle = "Put your life in hands of a Computer!";
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption handleAppOption={this.handleAppOption} />
+      </div>
+    );
+  }
 }
 
 IndecisionApp.defaultProps = {
-    options: []
+  options: []
 };
 
-const Header = (props)=> {
-        return (
-            <div>
-                <h1>{props.title}</h1>
-                {props.subtitle && <h2>{props.subtitle}</h2>}
-            </div>
-        );
+const Header = props => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
 };
 
 Header.defaultProps = {
-    title: 'Indecision'
+  title: "Indecision"
 };
 
-const Action = (props)=> {
-        return (
-            <div>
-                <button 
-                onClick = {props.handlePick}
-                disabled = {!props.hasOptions}
-                >
-                    What should I do?
-                </button>
-            </div>
-        );
+const Action = props => {
+  return (
+    <div>
+      <button onClick={props.handlePick} disabled={!props.hasOptions}>
+        What should I do?
+      </button>
+    </div>
+  );
 };
 
-const Options = (props)=> {
-        return (
-            <div>
-                <button onClick={props.handleDeleteOptions} >Remove All</button>
-                {
-                    props.options.map((option) => (
-                        <Option 
-                            key={option} 
-                            optionText={option} 
-                            handleDeleteOption={props.handleDeleteOption} 
-                        />
-                    ))
-                }
-            </div>
-        );
+const Options = props => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.map(option => (
+        <Option
+          key={option}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))}
+    </div>
+  );
 };
 
-const Option = (props) => {
-        return (
-            <div>
-                {props.optionText}
-                <button 
-                    onClick={(e)=>{
-                        props.handleDeleteOption(props.optionText)
-                    }} 
-                >
-                    Remove
-                </button>
-            </div>
-        );
+const Option = props => {
+  return (
+    <div>
+      {props.optionText}
+      <button
+        onClick={e => {
+          props.handleDeleteOption(props.optionText);
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
 };
 
 class AddOption extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleAppOption = this.handleAppOption.bind(this);
-        this.state = {
-            error: undefined
-        }
-    }
-    handleAppOption(e){
-        e.preventDefault();
-        const option = e.target.elements.option.value.trim();
-        // if(option){
-        //     alert(option);
-        // }
-        const error = this.props.handleAppOption(option);
-        this.setState(()=>({error}));
-    }
-    render(){
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleAppOption}>
-                    <input type='text' name='option' />
-                    <button>Add Option</button>
-                </form>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.handleAppOption = this.handleAppOption.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
+  handleAppOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    // if(option){
+    //     alert(option);
+    // }
+    const error = this.props.handleAppOption(option);
+    this.setState(() => ({ error }));
+  }
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAppOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-const User = (props) => {
-    return (
-        <div>
-            <p>Name: {props.name}</p>
-            <p>Age: {props.age} </p>
-        </div>
-    );
+const User = props => {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age} </p>
+    </div>
+  );
 };
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
