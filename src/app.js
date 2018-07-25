722a535f9637ac97f65c1868bcedb7e9da453aea
Lucifer <ramanandsirvi21@gmail.com>
2018-07-25 13:19:06 +0530

    New commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("app.js");
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
